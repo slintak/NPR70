8b18f8fad213212363b10edfbe2db4054d615dd1
Vlastimil Slinták <slintak@uart.cz>
2025-07-09 11:57:04 +0200

    Changed line endings from DOS to Unix format

--- NPR_14/source/main.cpp
+++ NPR_14/source/main.cpp
@@ -56,223 +56,223 @@ SPI_F4HDK spi_1(PB_5, PB_4, PB_3); // mosi, miso, sclk (radio)
 int main()
 {
     wait_ms(200);
-	pc.baud(115200);
+    pc.baud(115200);
     pc.printf("\r\n\r\nNPR FW %s\r\n", FW_VERSION);
-	
-	Timer slow_timer; 
-	int i = 1;
-	int temperature_timer = 0;
-	
-	static LAN_conf_T* LAN_conf_p;
-	LAN_conf_p = &LAN_conf_applied;
-	
-	static W5500_chip W5500_1;
-	W5500_p1 = &W5500_1;
-	W5500_1.spi_port = &spi_2;
+
+    Timer slow_timer;
+    int i = 1;
+    int temperature_timer = 0;
+
+    static LAN_conf_T* LAN_conf_p;
+    LAN_conf_p = &LAN_conf_applied;
+
+    static W5500_chip W5500_1;
+    W5500_p1 = &W5500_1;
+    W5500_1.spi_port = &spi_2;
     W5500_1.cs = &CS1;
-    W5500_1.interrupt = &Int_W5500; 
-	
+    W5500_1.interrupt = &Int_W5500;
+
 //#ifdef EXT_SRAM_USAGE
-	static ext_SRAM_chip SPI_SRAM;
-	SPI_SRAM_p = &SPI_SRAM;
-	SPI_SRAM.spi_port = &spi_2;
-	SPI_SRAM.cs = &CS3;
+    static ext_SRAM_chip SPI_SRAM;
+    SPI_SRAM_p = &SPI_SRAM;
+    SPI_SRAM.spi_port = &spi_2;
+    SPI_SRAM.cs = &CS3;
 //#endif
-	
-	static SI4463_Chip SI4463_1;
-	SI4463_1.spi = &spi_1;//1
-	SI4463_1.cs = &CS2;//2
-	SI4463_1.interrupt = &Int_SI4463;
-	SI4463_1.RX_LED = &LED_RX_loc;
-	SI4463_1.SDN = &SI4463_SDN;
-	
-	G_SI4463 = &SI4463_1;
-	
-	G_FDD_trig_pin = &FDD_trig_pin;
-	G_FDD_trig_IRQ = &FDD_trig_IRQ;
-	G_PTT_PA_pin = &PTT_PA_pin;
-	
-	reset_DHCP_table(LAN_conf_p);
-		
+
+    static SI4463_Chip SI4463_1;
+    SI4463_1.spi = &spi_1;//1
+    SI4463_1.cs = &CS2;//2
+    SI4463_1.interrupt = &Int_SI4463;
+    SI4463_1.RX_LED = &LED_RX_loc;
+    SI4463_1.SDN = &SI4463_SDN;
+
+    G_SI4463 = &SI4463_1;
+
+    G_FDD_trig_pin = &FDD_trig_pin;
+    G_FDD_trig_IRQ = &FDD_trig_IRQ;
+    G_PTT_PA_pin = &PTT_PA_pin;
+
+    reset_DHCP_table(LAN_conf_p);
+
     spi_2.format(8,0);
-	spi_1.format(8,0);
-	spi_2.frequency(20000000); 
-	spi_1.frequency(10000000);
-	
-	for(i=0; i<radio_addr_table_size; i++) {
-		CONF_radio_addr_table_status[i] = 0;
-	}
-	
-	G_FDD_trig_pin->input();
-	G_PTT_PA_pin->write(0);
-	LED_RX_loc = 0;
-	LED_connected = 0;
+    spi_1.format(8,0);
+    spi_2.frequency(20000000);
+    spi_1.frequency(10000000);
+
+    for(i=0; i<radio_addr_table_size; i++) {
+        CONF_radio_addr_table_status[i] = 0;
+    }
+
+    G_FDD_trig_pin->input();
+    G_PTT_PA_pin->write(0);
+    LED_RX_loc = 0;
+    LED_connected = 0;
     CS1=1;
-	CS2=1;
-	CS3=1;
-	SI4463_SDN = 1;
-	
-	wait_ms(20);
-	is_SRAM_ext = ext_SRAM_detect();
-	
-	LED_RX_loc = 1; 
-	for (i=0; i<7; i++) {
-		wait_ms(200);
-		LED_RX_loc = !LED_RX_loc;
-		LED_connected = !LED_connected;
-		SI4463_SDN = !SI4463_SDN;
-	}
-	LED_RX_loc = 0;
-	LED_connected = 0;
-	SI4463_SDN = 1;
-	
-	printf("\r\n");
+    CS2=1;
+    CS3=1;
+    SI4463_SDN = 1;
 
-	RX_FIFO_WR_point = 0;
-	RX_FIFO_RD_point = 0;
-	RX_FIFO_last_received = 0;
+    wait_ms(20);
+    is_SRAM_ext = ext_SRAM_detect();
 
-	//TXPS_FIFO->is_single = 0;
-	//TXPS_FIFO->WR_point = 0;
-	//TXPS_FIFO->RD_point = 0;
-	//TXPS_FIFO->last_ready = 0;
+    LED_RX_loc = 1;
+    for (i=0; i<7; i++) {
+        wait_ms(200);
+        LED_RX_loc = !LED_RX_loc;
+        LED_connected = !LED_connected;
+        SI4463_SDN = !SI4463_SDN;
+    }
+    LED_RX_loc = 0;
+    LED_connected = 0;
+    SI4463_SDN = 1;
 
-	GLOBAL_timer.reset();
-	GLOBAL_timer.start();
-	TDMA_init_all();
-	
-	//TXPS_FIFO->last_ready = TXPS_FIFO->WR_point;
-	for (i=0; i<0x2000; i++) {
-		RX_FIFO_data[i] = 0;
-	}
+    printf("\r\n");
 
-	printf("      \r\n");
-	for (i=0; i<radio_addr_table_size; i++) {
-		CONF_radio_addr_table_IP_begin[i] = 0; // 0 = entry not active
-		
-	}
-	if (is_SRAM_ext) {
-		printf("config WITH ext SRAM\r\n");
-	} else {
-		printf("config WITHOUT ext SRAM\r\n");
-	}
-	
-	wait_ms(100);
-	NFPR_config_read();
+    RX_FIFO_WR_point = 0;
+    RX_FIFO_RD_point = 0;
+    RX_FIFO_last_received = 0;
 
-	//SI4463_print_version(G_SI4463);//!!!!
-	SI4463_get_state(G_SI4463);
+    //TXPS_FIFO->is_single = 0;
+    //TXPS_FIFO->WR_point = 0;
+    //TXPS_FIFO->RD_point = 0;
+    //TXPS_FIFO->last_ready = 0;
 
-	i = SI4463_configure_all();
-	if (i == 1) {
-		HMI_printf("SI4463 configured\r\n");
-	} else {
-		HMI_printf("SI4463 error while configure\r\n");
-		SI4463_print_version(G_SI4463);
-		wait_ms(5000);
-		if (serial_term_loop() == 0) {//no serial char detected
-			NVIC_SystemReset(); //reboot
-		}
-	}
-	//SI4463_print_version(G_SI4463);//!!!!
-	
-	W5500_initial_configure(W5500_p1);
-	wait_ms(2);
+    GLOBAL_timer.reset();
+    GLOBAL_timer.start();
+    TDMA_init_all();
+
+    //TXPS_FIFO->last_ready = TXPS_FIFO->WR_point;
+    for (i=0; i<0x2000; i++) {
+        RX_FIFO_data[i] = 0;
+    }
+
+    printf("      \r\n");
+    for (i=0; i<radio_addr_table_size; i++) {
+        CONF_radio_addr_table_IP_begin[i] = 0; // 0 = entry not active
+
+    }
+    if (is_SRAM_ext) {
+        printf("config WITH ext SRAM\r\n");
+    } else {
+        printf("config WITHOUT ext SRAM\r\n");
+    }
+
+    wait_ms(100);
+    NFPR_config_read();
+
+    //SI4463_print_version(G_SI4463);//!!!!
+    SI4463_get_state(G_SI4463);
+
+    i = SI4463_configure_all();
+    if (i == 1) {
+        HMI_printf("SI4463 configured\r\n");
+    } else {
+        HMI_printf("SI4463 error while configure\r\n");
+        SI4463_print_version(G_SI4463);
+        wait_ms(5000);
+        if (serial_term_loop() == 0) {//no serial char detected
+            NVIC_SystemReset(); //reboot
+        }
+    }
+    //SI4463_print_version(G_SI4463);//!!!!
+
+    W5500_initial_configure(W5500_p1);
+    wait_ms(2);
 #ifdef EXT_SRAM_USAGE
-	ext_SRAM_set_mode(SPI_SRAM_p);
-	wait_ms(2)
-	ext_SRAM_init();
+    ext_SRAM_set_mode(SPI_SRAM_p);
+    wait_ms(2)
+    ext_SRAM_init();
 #endif
-	TDMA_NULL_frame_init(70);
-	
-	SI4463_periodic_temperature_check(G_SI4463);
-	Int_SI4463.fall(&SI4463_HW_interrupt);
-	if (CONF_radio_default_state_ON_OFF) {
-		//TDMA_init_all();
-		//SI4463_radio_start();
-		RADIO_on(1, 0, 1);//init state, no reconfigure, HMI output
-	}
-	HMI_printf("ready> ");
-	slow_timer.start();
+    TDMA_NULL_frame_init(70);
 
-	unsigned int timer_snapshot;
-	int slow_action_counter = 0;
-	int signaling_counter = 0;
-	//SI4463_temp_check_init();
-	
-	int telnet_counter = 0;
-	
-	while(1) {	
-		for (i=0; i<100; i++) {
-			if ( (is_TDMA_master == 1) && (CONF_master_FDD == 2) ) {
-				FDDup_RX_FIFO_dequeue();
-			} else {
-				radio_RX_FIFO_dequeue(W5500_p1);
-			}
-			Eth_RX_dequeue(W5500_p1); 
-			TDMA_slave_timeout();
+    SI4463_periodic_temperature_check(G_SI4463);
+    Int_SI4463.fall(&SI4463_HW_interrupt);
+    if (CONF_radio_default_state_ON_OFF) {
+        //TDMA_init_all();
+        //SI4463_radio_start();
+        RADIO_on(1, 0, 1);//init state, no reconfigure, HMI output
+    }
+    HMI_printf("ready> ");
+    slow_timer.start();
+
+    unsigned int timer_snapshot;
+    int slow_action_counter = 0;
+    int signaling_counter = 0;
+    //SI4463_temp_check_init();
+
+    int telnet_counter = 0;
+
+    while(1) {
+        for (i=0; i<100; i++) {
+            if ( (is_TDMA_master == 1) && (CONF_master_FDD == 2) ) {
+                FDDup_RX_FIFO_dequeue();
+            } else {
+                radio_RX_FIFO_dequeue(W5500_p1);
+            }
+            Eth_RX_dequeue(W5500_p1);
+            TDMA_slave_timeout();
 #ifdef EXT_SRAM_USAGE
-			ext_SRAM_periodic_call();
+            ext_SRAM_periodic_call();
 #endif
-			if (is_SRAM_ext == 1) {
-				ext_SRAM_periodic_call();
-			}
-			timer_snapshot = slow_timer.read_us();
-			if (timer_snapshot > 666000) {//666000
-				slow_timer.reset();
-				slow_action_counter++;
-				if (slow_action_counter > 2) {slow_action_counter = 0; }
-				
-				if (slow_action_counter == 0) {
-					HMI_periodic_call();
-				}
-				//debug_counter = 0;
+            if (is_SRAM_ext == 1) {
+                ext_SRAM_periodic_call();
+            }
+            timer_snapshot = slow_timer.read_us();
+            if (timer_snapshot > 666000) {//666000
+                slow_timer.reset();
+                slow_action_counter++;
+                if (slow_action_counter > 2) {slow_action_counter = 0; }
 
-				
-				if (slow_action_counter == 1) {//every 2 sec
-					signaling_counter++;
-					if (signaling_counter >= CONF_signaling_period) {
-						signaling_periodic_call();
-						signaling_counter = 0;
-					}
-				}
-				
-				if (slow_action_counter == 2) {
-					DHCP_ARP_periodic_free_table();
-					W5500_re_configure_periodic_call(W5500_p1);
-				
-					temperature_timer++;
-					if(temperature_timer > 15) {// 15 every 30 sec
-						temperature_timer = 0;
-						G_need_temperature_check = 1;
-						SI4463_periodic_temperature_check_2();//SI4463_periodic_temperature_check(G_SI4463);
-					}
-				}
-			}
-		}
-		
-		if (is_TDMA_master) {
-			if (CONF_radio_state_ON_OFF==1) {LED_connected.write((timer_snapshot >> 19) & 1);}
-			else {LED_connected.write(0);}
-		} else {
-			if (my_client_radio_connexion_state == 2) {
-				LED_connected.write(1);
-			}else{
-				LED_connected.write(0);
-			}
-		}
-		if (is_telnet_active) {
-			
-			telnet_counter++;
-			if (telnet_counter>10) {
-				telnet_loop(W5500_p1);
-				telnet_counter = 0;
-			}
-		}
-		serial_term_loop();
-		
-		if ( (LAN_conf_applied.DHCP_server_active == 1) && (is_TDMA_master == 0) ) {
-			DHCP_server(LAN_conf_p, W5500_p1);
-		}
-	}
+                if (slow_action_counter == 0) {
+                    HMI_periodic_call();
+                }
+                //debug_counter = 0;
+
+
+                if (slow_action_counter == 1) {//every 2 sec
+                    signaling_counter++;
+                    if (signaling_counter >= CONF_signaling_period) {
+                        signaling_periodic_call();
+                        signaling_counter = 0;
+                    }
+                }
+
+                if (slow_action_counter == 2) {
+                    DHCP_ARP_periodic_free_table();
+                    W5500_re_configure_periodic_call(W5500_p1);
+
+                    temperature_timer++;
+                    if(temperature_timer > 15) {// 15 every 30 sec
+                        temperature_timer = 0;
+                        G_need_temperature_check = 1;
+                        SI4463_periodic_temperature_check_2();//SI4463_periodic_temperature_check(G_SI4463);
+                    }
+                }
+            }
+        }
+
+        if (is_TDMA_master) {
+            if (CONF_radio_state_ON_OFF==1) {LED_connected.write((timer_snapshot >> 19) & 1);}
+            else {LED_connected.write(0);}
+        } else {
+            if (my_client_radio_connexion_state == 2) {
+                LED_connected.write(1);
+            }else{
+                LED_connected.write(0);
+            }
+        }
+        if (is_telnet_active) {
+
+            telnet_counter++;
+            if (telnet_counter>10) {
+                telnet_loop(W5500_p1);
+                telnet_counter = 0;
+            }
+        }
+        serial_term_loop();
+
+        if ( (LAN_conf_applied.DHCP_server_active == 1) && (is_TDMA_master == 0) ) {
+            DHCP_server(LAN_conf_p, W5500_p1);
+        }
+    }
 }
